86dde168c5615f72019ec5d8fc7c8995d6aaedea
Yashaswini Naik <yashaswininaik@Yashaswinis-MacBook-Pro.local>
2024-09-04 22:20:11 +0530

    Basic test cases

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import unittest
 
 class StringCalculator:
     def add(self, numbers: str) -> int:
-        pass
+        if not numbers:
+            return 0
+
+        numbers = [int(num) for num in numbers.split(',')]
+        return sum(numbers)
+
+
+class TestStringCalculator(unittest.TestCase):
+    def test_empty_string(self):
+        calc = StringCalculator()
+        self.assertEqual(calc.add(""), 0)
+
+    def test_comma_separated_string(self):
+        calc = StringCalculator()
+        self.assertEqual(calc.add("1,2,3"), 6)
+
+if __name__ == "__main__":
+    unittest.main()
